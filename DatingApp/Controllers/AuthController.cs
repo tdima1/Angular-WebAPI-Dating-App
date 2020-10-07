ae0a08b2a712190d74267e4643dd1370ff3e8007
Dima Tiberiu <dima.tiberiu1@gmail.com>
2020-10-07 14:57:18 +0300

    Added Register functionality. AuthController - checks if user exists the stores username/password in db. pass is hashed. Added UserModel and DTOs. Added AuthRepo

---
+++ DatingApp/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.DTOs;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+      private readonly IAuthRepository _authRepository;
+
+      public AuthController(IAuthRepository authRepository)
+      {
+         _authRepository = authRepository;
+      }
+
+      [HttpPost("register")]
+      public async Task<IActionResult> Register(UserForRegisterDTO userForRegisterDTO)
+      {
+         //validate request
+
+         userForRegisterDTO.Username = userForRegisterDTO.Username.ToLower();
+         if (await _authRepository.UserExists(userForRegisterDTO.Username)) {
+            return BadRequest("Username already exists");
+         }
+
+         var userToCreate = new User() {
+            Username = userForRegisterDTO.Username
+         };
+
+         var createdUser = await _authRepository.Register(userToCreate, userForRegisterDTO.Password);
+         return StatusCode(201);
+      }
+
+    }
+}
---
+++ DatingApp/DTOs/UserForRegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.DTOs
+{
+   public class UserForRegisterDTO
+   {
+      [Required]
+      public string Username { get; set; }
+
+      [Required]
+      [StringLength(8, MinimumLength = 4, ErrorMessage ="Pass between 4-8")]
+      public string Password { get; set; }
+   }
+}
